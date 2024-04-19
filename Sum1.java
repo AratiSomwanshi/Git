4e6d9f9556decfd9b1f631ab9549f66280a6a961
Arati Somwanshi <7arati10@gmail.com>
2024-04-19 12:26:23 +0530

    Initail commit

---
+++ Sum1.java
@@ -0,0 +1,28 @@
+class Sum {
+    static int sum(int n){
+        if(n==0)
+        return 0;
+return (n%10)+ sum(n/10);
+    }
+    public static void main(String[] args) {
+        int num=123456;
+        System.out.println(sum(num));
+        
+        
+        
+        
+        /*int sum=0;
+        int num=123456;
+        for(int i=0; i<=10; i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+        while(num!=0){
+            sum+=num%10;
+            num=num/10;
+        }
+        System.out.println(sum);
+    }*/
+    
+}
+}
