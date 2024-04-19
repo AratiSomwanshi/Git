e0aed53001919bb9720aea99360e80f577fbfdeb
Arati Somwanshi <7arati10@gmail.com>
2024-04-19 13:08:26 +0530

    Initial commit

--- Sum1.java
+++ Sum1.java
@@ -1,12 +1,12 @@
 class Sum {
-    static int sum(int n){
-        if(n==0)
-        return 0;
-return (n%10)+ sum(n/10);
-    }
-    public static void main(String[] args) {
-        int num=123456;
-        System.out.println(sum(num));
+//     static int sum(int n){
+//         if(n==0)
+//         return 0;
+// return (n%10)+ sum(n/10);
+//     }
+//     public static void main(String[] args) {
+//         int num=123456;
+//         System.out.println(sum(num));
         
         
         
